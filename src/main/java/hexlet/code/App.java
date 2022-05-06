f9d9a86239cc0b4b1981d3d58b4d06b05bae3853
vlad.smelianets <vlad.smelianets@yandex.ru>
2022-05-06 13:17:11 +0300

    refactor, fix: rework main menu with switch, make Cli.greet() void, upgrade gradle ver

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,15 +2,13 @@ package hexlet.code;
 
 import hexlet.code.games.CalcGame;
 import hexlet.code.games.EvenGame;
-import hexlet.code.games.Game;
 import hexlet.code.games.GcdGame;
 import hexlet.code.games.PrimeGame;
 import hexlet.code.games.ProgressionGame;
-import hexlet.code.utils.CliHelper;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
+import static java.lang.String.format;
 import static java.lang.System.lineSeparator;
 import static java.lang.System.out;
 
@@ -18,36 +16,48 @@ public class App {
 
     public static final int EXIT = 0;
     public static final int GREET = 1;
+    private static final int EVEN = 2;
+    private static final int CALC = 3;
+    private static final int GCD = 4;
+    private static final int PROGRESSION = 5;
+    private static final int PRIME = 6;
 
-    private static final int TWO = 2;
-    private static final int THREE = 3;
-    private static final int FOUR = 4;
-    private static final int FIVE = 5;
-    private static final int SIX = 6;
+    private static final String MENU = "Please enter the game number and press Enter." + lineSeparator()
+            + format("%d - Greet%s", GREET, lineSeparator())
+            + format("%d - Even%s", EVEN, lineSeparator())
+            + format("%d - Calc%s", CALC, lineSeparator())
+            + format("%d - GCD%s", GCD, lineSeparator())
+            + format("%d - Progression%s", PROGRESSION, lineSeparator())
+            + format("%d - Prime%s", PRIME, lineSeparator())
+            + format("%d - Exit%s", EXIT, lineSeparator());
+
+    private static final String DEFAULT_PLAYER = "UnknownPlayer";
 
     public static void main(String[] args) {
+        out.print(MENU);
+        int selection = new Scanner(System.in).nextInt();
 
-        Map<Integer, Game> games = new HashMap<>();
-        games.put(TWO, new EvenGame());
-        games.put(THREE, new CalcGame());
-        games.put(FOUR, new GcdGame());
-        games.put(FIVE, new ProgressionGame());
-        games.put(SIX, new PrimeGame());
-
-        int selection = -1;
-
-        out.println("Please enter the game number and press Enter.");
-        out.printf("%d - Greet%s", GREET, lineSeparator());
-        games.forEach((key, value) -> out.printf("%d - %s%s", key, value.getName(), lineSeparator()));
-        out.printf("%d - Exit%s", EXIT, lineSeparator());
-
-        selection = CliHelper.readInt("Your choice: ");
-
-        if (selection == GREET) {
-            Cli.greet();
-        } else if (selection != EXIT) {
-            String playerName = Cli.greet();
-            new Engine(playerName).start(games.get(selection));
+        switch (selection) {
+            case (GREET):
+                Cli.greet();
+                break;
+            case (EVEN):
+                new Engine(DEFAULT_PLAYER).start(new EvenGame());
+                break;
+            case (CALC):
+                new Engine(DEFAULT_PLAYER).start(new CalcGame());
+                break;
+            case (GCD):
+                new Engine(DEFAULT_PLAYER).start(new GcdGame());
+                break;
+            case (PROGRESSION):
+                new Engine(DEFAULT_PLAYER).start(new ProgressionGame());
+                break;
+            case (PRIME):
+                new Engine(DEFAULT_PLAYER).start(new PrimeGame());
+                break;
+            default:
+                break;
         }
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -7,14 +7,12 @@ public final class Cli {
     private Cli() {
     }
 
-    public static String greet() {
+    public static void greet() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(System.lineSeparator() + "Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
-
-        return userName;
     }
 }
